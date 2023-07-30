e7c161313e50bb4c2c18a5fe2ee8e94dab9e72ba
Muhammad <Muhammad.medhat@gmail.com>
2023-07-30 18:25:09 +0300

    Adding placeorder and delivery screens

--- food-ordering-client/App/Navigation.js
+++ food-ordering-client/App/Navigation.js
@@ -6,6 +6,9 @@ import React from "react";
 import HomeScreen from "./screens/HomeScreen";
 import TestScreen from "./screens/TestScreen";
 import RestaurantScreen from "./screens/RestaurantScreen";
+import CartScreen from "./screens/CartScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import DeliveryScreen from "./screens/DeliveryScreen";
 
 const Navigation = () => {
   return (
@@ -16,6 +19,21 @@ const Navigation = () => {
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Test" component={TestScreen} />
         <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+        <Stack.Screen
+          name="Cart"
+          options={{ presentation: "modal" }}
+          component={CartScreen}
+        />
+        <Stack.Screen
+          name="PlaceOrder"
+          options={{ presentation: "fullScreenModal" }}
+          component={PlaceOrderScreen}
+        />
+        <Stack.Screen
+          name="Delivery"
+          options={{ presentation: "modal" }}
+          component={DeliveryScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
